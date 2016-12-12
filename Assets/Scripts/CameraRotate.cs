33962c400781139dd913322c9db34d95107c04dc
Eric Thompson <eric.r.thompson@icloud.com>
2016-12-12 03:44:30 -0500

    Added main menu

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour {
+
+    private Transform camPos;
+
+	// Use this for initialization
+	void Start () {
+        camPos = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        camPos.Rotate(new Vector3(0f, 0.1f, 0f));
+	}
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public void LoadLevel (string levelName) {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame () {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+}
